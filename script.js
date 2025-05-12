7f2c09066e8a4629ada1e664cc12920d7f0d8db3
Talha Faizan <talhafaizan508@gmail.com>
2025-05-12 22:56:42 +0500

    JS-Assignment-3

---
+++ script.js
@@ -0,0 +1,29 @@
+Single Spaces
+
+let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  let count = 0;
+
+  for (let i = 0; i < paragraph.length; i++) {
+    if (paragraph[i] === "  ") {
+      count++;
+    }
+  }
+
+  console.log("Paragraph: " + paragraph);
+  console.log("Total spaces: " + count);
+
+  // Double Spaces
+
+//   let paragraph = "Lorem  ipsum dolor  sit amet,  consectetur  adipiscing elit.";
+//   let count = 0;
+
+//   for (let i = 0; i < paragraph.length - 1; i++) {
+//     if (paragraph[i] === " " && paragraph[i + 1] === " ") {
+//       count++;
+//     }
+//   }
+
+//   console.log("Paragraph: " + paragraph);
+//   console.log("Total double spaces: " + count);
+
+
