bf96a2150a8e11e2782ed81da5fb1791daec5216
Talha Faizan <140506077+TalhaFaizan@users.noreply.github.com>
2025-05-16 00:01:48 +0500

    JS-Assignment-3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Single Spaces
+//Single Spaces
 
 let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
   let count = 0;
